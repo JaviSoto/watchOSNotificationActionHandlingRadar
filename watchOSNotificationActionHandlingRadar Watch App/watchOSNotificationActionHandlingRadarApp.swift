673fe9a05df1722f487084b3f5cf58501c5a8804
Javier Soto <ios@javisoto.es>
2022-09-03 14:48:25 -0700

    Add work around and comments

--- watchOSNotificationActionHandlingRadar Watch App/watchOSNotificationActionHandlingRadarApp.swift
+++ watchOSNotificationActionHandlingRadar Watch App/watchOSNotificationActionHandlingRadarApp.swift
@@ -14,6 +14,8 @@ private func path(from notificationResponse: UNNotificationResponse) -> Navigati
     return .init([identifierFromPayload ?? 0])
 }
 
+/// A class implementing `UNUserNotificationCenterDelegate` in the natural way to be able to update a binding to `navigationPath`, whcih in fact does not work.
+// Note: Without this main actor, if we perform `async` tasks in the `didReceive` method, the app crashes deep inside Apple's frameworks.
 @MainActor
 private final class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
     init(navigationPath: Binding<NavigationPath>) {
@@ -33,12 +35,34 @@ private final class NotificationDelegate: NSObject, UNUserNotificationCenterDele
     }
 }
 
+/// A hacky approach to implement `UNUserNotificationCenterDelegate` in order to be able to update `navigationPath` when handling a notification.
+@MainActor
+private final class NotificationDelegateWorkaround: NSObject, UNUserNotificationCenterDelegate {
+    var handleResponse: ((UNNotificationResponse) -> Void)?
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        // We have to wait for the app UI to be set up,
+        // since `navigateToDestination` is set in `onAppear`,
+        // and that's the only way I could get it to work.
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            if let handleResponse = self.handleResponse {
+                handleResponse(response)
+            } else {
+                print("Error: Notification response not set when received notification response")
+            }
+        }
+    }
+}
+
+
+
 @main
 struct watchOSNotificationActionHandlingRadar_Watch_AppApp: App {
     @State
     var navigationPath = NavigationPath()
     
     private var notificationDelegate: NotificationDelegate!
+    private var notificationDelegateWorkaround: NotificationDelegateWorkaround!
     
     init() {
         // Note: There's 0 documentation that explains how to handle notification actions in SwiftUI watchOS apps,
@@ -46,6 +70,12 @@ struct watchOSNotificationActionHandlingRadar_Watch_AppApp: App {
         let delegate = NotificationDelegate(navigationPath: $navigationPath)
         UNUserNotificationCenter.current().delegate = delegate
         self.notificationDelegate = delegate
+        
+        let workaroundDelegate = NotificationDelegateWorkaround()
+        self.notificationDelegateWorkaround = workaroundDelegate
+        
+        // Uncomment this to enable the workaround:
+        // UNUserNotificationCenter.current().delegate = workaroundDelegate
     }
     
     var body: some Scene {
@@ -55,6 +85,11 @@ struct watchOSNotificationActionHandlingRadar_Watch_AppApp: App {
                     .navigationDestination(for: Int.self) { integer in
                         DetailScreen(integer: integer)
                     }
+            }
+            .onAppear {
+                notificationDelegateWorkaround.handleResponse = {
+                    navigationPath = path(from: $0)
+                }
             }
             .onChange(of: navigationPath) { newNavigationPath in
                 // This doesn't get called when `NotificationDelegate` updates `navigationPath`.
