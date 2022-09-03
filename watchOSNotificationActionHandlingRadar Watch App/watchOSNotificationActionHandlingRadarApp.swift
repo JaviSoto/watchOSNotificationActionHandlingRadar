fdeb120aabb4c3276741e5b80dfa616a53534a08
Javier Soto <ios@javisoto.es>
2022-09-03 14:22:16 -0700

    Initial Commit

---
+++ watchOSNotificationActionHandlingRadar Watch App/watchOSNotificationActionHandlingRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSNotificationActionHandlingRadarApp.swift
+//  watchOSNotificationActionHandlingRadar Watch App
+//
+//  Created by Javier Soto on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct watchOSNotificationActionHandlingRadar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
